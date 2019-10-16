511da42318662d6eafe37ac72c4669c98b053eb4
jmoralesg <johanamgff@gmail.com>
2019-10-16 16:00:48 +1100

    refactorizacion de la tarea

--- tareas/clase-5/tarea-4-solucion/tarea4.js
+++ tareas/clase-5/tarea-4-solucion/tarea4.js
@@ -6,54 +6,76 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
-const $listaDeLis = document.querySelectorAll('li');
-
-let promedioElem = document.querySelector('#promedio');
-const numMenorElem = document.querySelector('#numero-menor');
-const numMayorElem = document.querySelector('#numero-mayor');
-const numMasFrecuenteElem = document.querySelector('#numero-mas-frecuente');
+const $listaDeNumeros = document.querySelectorAll('li');
+let $promedio = document.querySelector('#promedio');
+const $numMenor = document.querySelector('#numero-menor');
+const $numMayor = document.querySelector('#numero-mayor');
+const $numMasFrecuente = document.querySelector('#numero-mas-frecuente');
 
 let listaDeNum = [];
 let sumaTotal = 0;
 
-for (let i = 0; i < $listaDeLis.length; i++) {
-  let valorDeLi = Number($listaDeLis[i].textContent);
-  listaDeNum.push(valorDeLi);
-}
-
-let menorNum = listaDeNum[1];
-let mayorNum = 0;
-//Promedio
-for (let i = 0; i < listaDeNum.length; i++) {
-  sumaTotal += listaDeNum[i];
-  //Numero Menor
-  if (menorNum > listaDeNum[i]) {
-    menorNum = listaDeNum[i];
-  }
-  //Numero Mayor
-  if (mayorNum < listaDeNum[i]) {
-    mayorNum = listaDeNum[i];
+function arregloDeLaLista() {
+  for (let i = 0; i < $listaDeNumeros.length; i++) {
+    let valorDeLi = Number($listaDeNumeros[i].textContent);
+    listaDeNum.push(valorDeLi);
   }
 }
-promedioElem.textContent = sumaTotal / listaDeNum.length;
-numMenorElem.textContent = menorNum;
-numMayorElem.textContent = mayorNum;
 
-//Mas frecuente
-let maxFrecuencia = 1;
-let contador = 0;
-let numMasFrecuente;
+function calculaPromedio() {
+  for (let i = 0; i < listaDeNum.length; i++) {
+    sumaTotal += listaDeNum[i];
+  }
+  $promedio.textContent = sumaTotal / listaDeNum.length;
+}
 
-for (let i = 0; i < listaDeNum.length; i++) {
-  for (let j = 0; j < listaDeNum.length; j++) {  
-    if (listaDeNum[i] === listaDeNum[j]) { 
-      contador += 1; 
-      if (contador > maxFrecuencia) { 
-        maxFrecuencia = contador; 
-        numMasFrecuente = listaDeNum[i];  
-      }
+function calculaNumeroMenor() {
+  let menorNum = listaDeNum[1];
+  for (let i = 0; i < listaDeNum.length; i++) {
+    if (menorNum > listaDeNum[i]) {
+      menorNum = listaDeNum[i];
     }
   }
-  contador = 0;
+  $numMenor.textContent = menorNum;
 }
-numMasFrecuenteElem.textContent = numMasFrecuente;
+
+function calculaNumeroMayor() {
+  let mayorNum = 0;
+  for (let i = 0; i < listaDeNum.length; i++) {
+    sumaTotal += listaDeNum[i];
+    if (mayorNum < listaDeNum[i]) {
+      mayorNum = listaDeNum[i];
+    }
+  }
+  $numMayor.textContent = mayorNum;
+}
+
+function calculaNumeroMasFrecuente() {
+  let maxFrecuencia = 1;
+  let contador = 0;
+  let numMasFrecuente;
+
+  for (let i = 0; i < listaDeNum.length; i++) {
+    for (let j = 0; j < listaDeNum.length; j++) {
+      if (listaDeNum[i] === listaDeNum[j]) {
+        contador += 1;
+        if (contador > maxFrecuencia) {
+          maxFrecuencia = contador;
+          numMasFrecuente = listaDeNum[i];
+        }
+      }
+    }
+    contador = 0;
+  }
+  $numMasFrecuente.textContent = numMasFrecuente;
+}
+
+function totales() {
+  arregloDeLaLista();
+  calculaPromedio();
+  calculaNumeroMenor();
+  calculaNumeroMayor();
+  calculaNumeroMasFrecuente();
+}
+
+totales();
